fa7861de060d291f751ebb0932bbe539a8c5ceb5
Ben <benbav97@gmail.com>
2024-11-07 20:16:21 -0600

    lst updates

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ import platform
 import random
 import os 
 
-lst = ['its fucking april fools fuck shit fuck ass fuck', 'fuck fuck, oh fuck', 'shit fucking shit ass fuck', 'april fools, happy april fools']
-
+lst = [
+    'its fucking april fools fuck shit fuck ass fuck', 
+    'fuck fuck, oh fuck', 
+    'shit fucking shit ass fuck', 
+    'april fools, happy april fools',
+    'god damn it, april fools time, fuck yeah',
+    'oh shit, fucking fuck butthole shit poop ass april fools, motherfucker',
+    'it’s april fucking fools god damn motherfucker shit poop all over the floor',
+    'what the fucking hell, it’s april fools day, shit god damn motherfucking shit fuck',
+    'no way, april fools, you’re fucked now',
+    'shit fucking god damn motherfucker, it’s april fools'
+]
 def main():
     output = random.choice(lst)
     if platform.system() == 'Darwin': # only works on MacOS
