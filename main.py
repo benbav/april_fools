3d4229be684f80d77a625fcf30c67e5ebc5bbcab
Ben <benbav97@gmail.com>
2024-11-07 20:05:50 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+import os 
+
+lst = ['its fucking april fools fuck shit fuck ass fuck', 'fuck fuck, oh fuck', 'shit fucking shit ass fuck', 'april fools, happy april fools']
+
+def main():
+    output = random.choice(lst)
+    os.system('say ' + output)
+
+if __name__ == "__main__":
+    main()
+
+# crontab schedule every hour on the 1st of april
+# (crontab -l ; echo "0 0-23 1 4 * python3 main.py") | crontab -
